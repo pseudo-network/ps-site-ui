559dc92f08a6329437c51880ddb13e4d556fef52
rrossilli55 <rrossilli55@gmail.com>
2021-07-23 10:35:21 -0400

    progress

--- core/environments.js
+++ core/environments.js
@@ -1,9 +1,10 @@
-export const API_BASE_URL = "http://api.pseudonetwork.net:3444"
-export const CHART_BASE_URL = "http://charts.pseudonetwork.net"
+export const API_BASE_URL = "http://api.pseudocoin.app/v1"
 
 // bitquery
 export const BITQUERY_BASE_URL = "https://graphql.bitquery.io"
 export const BITQUERY_API_KEY = "BQYug1u2azt1EzuPggXfnhdhzFObRW0g"
+
+//coins
 export const ETHEREUM_ADDRESS = "0xdac17f958d2ee523a2206206994597c13d831ec7"
 export const USDC_ADDRESS = "0x8ac76a51cc950d9822d68b83fe1ad97b32cd580d"
 export const WBNB_ADDRESS = "0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c"
