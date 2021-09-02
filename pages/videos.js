cf3073c65da3af87778cd089e8fd6591134b7a6c
rrossilli55 <rrossilli55@gmail.com>
2021-09-02 10:36:46 -0400

    added videos

--- pages/videos.js
+++ pages/videos.js
@@ -34,6 +34,27 @@ export default function Home() {
               url={"https://www.youtube.com/embed/IN0TWRCZbgc"}
             />
           </Grid>
+          <Grid container item xs={12} sm={6} md={3} spacing={1}>
+            <PSVideoCard
+              title={"How to Buy and Sell Pseudocoin and Other BSC tokens?"}
+              url={"https://www.youtube.com/embed/VbqosA2HvuM"}
+            />
+          </Grid>
+        </Grid>
+
+        <br />
+
+        <Typography>Livesteams</Typography>
+
+        <br />
+
+        <Grid container spacing={2}>
+          <Grid container item xs={12} sm={6} md={3} spacing={1}>
+            <PSVideoCard
+              title={"August 10, 2021 AMA"}
+              url={"https://www.youtube.com/embed/GfKeOmIld5M"}
+            />
+          </Grid>
         </Grid>
 
         <br />
@@ -55,18 +76,12 @@ export default function Home() {
               url={"https://www.youtube.com/embed/9opEiMKL0GU"}
             />
           </Grid>
-          {/* <Grid container item xs={12} sm={6} md={3} spacing={1}>
-            <PSVideoCard
-              title={"How to Set up MetaMask Wallet"}
-              url={"https://www.youtube.com/embed/hAqpb7aNSt0"}
-            />
-          </Grid>
           <Grid container item xs={12} sm={6} md={3} spacing={1}>
             <PSVideoCard
-              title={"Alt Coin Trading: How to Spot a Scam Token"}
-              url={"https://www.youtube.com/embed/vNOfWhTPG_Q"}
+              title={"What are Stablecoins?"}
+              url={"https://www.youtube.com/embed/Eqm9bOSOUVs"}
             />
-          </Grid> */}
+          </Grid>
         </Grid>
       </NavFrame>
     </>
